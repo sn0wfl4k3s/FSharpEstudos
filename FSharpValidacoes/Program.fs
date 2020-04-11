85d9d6d4a9436aa55e6064b18620af9b7666d196
Flakes <ed.afram@gmail.com>
2020-04-11 13:31:16 -0300

    Inclusão de validação de cpf.

---
+++ FSharpValidacoes/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Validacoes
+
+[<EntryPoint>]
+let main argv =
+    
+    let cpf = "12345678909"
+
+    let SeValido cpf =
+        let valido = cpfValido cpf
+        match valido with
+        | Some n ->
+            if n = true then
+                printfn "O cpf é válido!"
+            else
+                printfn "O cpf não é válido!"
+        | None ->
+            printf "O cpf não está num formato válido!"
+    
+    SeValido cpf
+
+
+    0 // return an integer exit code
---
+++ FSharpValidacoes/Validacoes.fs
@@ -0,0 +1,19 @@
+﻿module Validacoes
+
+    let cpfValido (cpf: string) =
+        match cpf with
+        | _cpf when _cpf.Length = 11 || _cpf.Length = 14 ->
+            let cpfFiltrado = cpf.Replace("-", "").Replace(".", "")
+            let cpfNumerico = 
+                Seq.toList cpfFiltrado
+                |> List.map (fun c -> c |> string |> System.Int32.Parse)
+            let tuplaOitoDigitosInverso = 
+                Seq.toList cpfNumerico.[..8]
+                |> List.rev
+                |> List.mapi (fun i c -> (i, c ))
+            let s1 = List.sumBy (fun (i, cpf) ->  cpf * (9 - (i % 10))) tuplaOitoDigitosInverso
+            let s2 = List.sumBy (fun (i, cpf) ->  cpf * (9 - ((i + 1) % 10))) tuplaOitoDigitosInverso
+            let d1 = (s1 % 11) % 10;
+            let d2 = ((s2 + d1 * 9) % 11) % 10
+            Some([d1; d2] = cpfNumerico.[9..])
+        | _ -> None
