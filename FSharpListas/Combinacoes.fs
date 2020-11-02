fbec5c3b316f39fc48b3e52a74f95e278c4cf151
Flakes <ed.afram@gmail.com>
2020-11-02 19:21:28 -0300

    inclusão

--- FSharpListas/Combinacoes.fs
+++ FSharpListas/Combinacoes.fs
@@ -20,3 +20,22 @@
         | head::tail -> 
             let headMoreRest = combinacoesDe3 tail |> List.map (fun comb3 -> [head] @ comb3)
             headMoreRest @ (combinacoesDe4 tail)
+
+    let rec combinacoesDe (quantidade:int) (l:array<'T>) =
+        let lista = l |> Array.toList
+        match lista with
+        | _lista when (List.length _lista) >= quantidade  -> 
+            match quantidade with
+            | 2 ->
+                match lista with
+                | [] -> []
+                | head::tail ->
+                    let headAndMore = tail |> List.map (fun e -> [head; e])
+                    headAndMore @ (tail |> List.toArray |> CombinacoesDe 2)
+            | _ -> 
+                match lista with
+                | [] -> []
+                | head::tail ->
+                    let headAndMore = CombinacoesDe (quantidade - 1) (List.toArray tail) |> List.map(fun c -> [head] @ c)
+                    headAndMore @ (tail |> List.toArray |> CombinacoesDe quantidade)
+        | _ -> []
